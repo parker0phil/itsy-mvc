087017ca4f93f8cfb38785e8ed52e96064d8c288
Phil Parker <parker.p.dev@gmail.com>
2011-03-17 22:53:53 +0000

    Added header and querystring parsing

--- itsy-mvc.groovy
+++ itsy-mvc.groovy
@@ -1,5 +1,12 @@
 import groovy.text.*
-if (!init && line.size() == 0) {
-        out << new GStringTemplateEngine().createTemplate(new File("view.html").newReader("UTF-8")).make(evaluate(new File("controller.groovy"))).toString()
-        return "success"
+if (init){ args = ["URL" : (line.split(" ")[1]) ]}
+else {
+	if (line.size() > 0){
+		args.put(line.substring(0, line.indexOf(":")).replace('-',''), line.substring(line.indexOf(":")+2)) 
+    		args.put("params", args.URL.contains("?") ? args.URL.split('\\?')[1].split('&').inject([:]){ map, qstr ->  qstr.split('=').with { arg -> map[arg[0]] = arg[1] } ; map } : [:] )
+   		args.put("contexts", (args.URL.contains("?") ? args.URL.split('\\?')[0].split('/') : args.URL.split('/')))
+	}else{
+        	out << new GStringTemplateEngine().createTemplate(new File("view.html").newReader("UTF-8")).make(args+evaluate(new File("controller.groovy"))).toString()
+        	return "success"
+	}
 }
